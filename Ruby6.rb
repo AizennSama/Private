f4e8b50bb77152083da0f52ec40c426acf5e0850
Jeevan <164208260+AizennSama@users.noreply.github.com>
2024-08-10 13:20:24 +0530

    Updated and renamed prgm6.rb to Ruby6.rb

---
+++ Ruby6.rb
@@ -0,0 +1,7 @@
+i=10 
+while i>0 
+  if (i%2 != 0)  #whether even or not
+    puts i
+  end
+i -=1    #decrement i after every iteration
+end
--- prgm6.rb
+++
@@ -1,5 +0,0 @@
-10.downto(1) do|num|
-    if num%2 != 0
-        puts num
-    end
-end
